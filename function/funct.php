e5d0b1d2ea171c0096193843008fb3082088a212
rutvik <rutvikamlani23@gmail.com>
2024-02-07 04:03:57 +0300

    update

--- function/funct.php
+++
@@ -1,9 +0,0 @@
-<?php
-function hello_world(){
-    $firstname = 'bob';
-    $salary = 398.5;
-    $is_married = true;
- echo "first name : $firstname salary: $salary  are you married: $is_married ";
-}
-hello_world();
-?>
--- variables/user.php
+++
@@ -1,9 +0,0 @@
-<?php
- $name = "Rutvik Amlani";
- $age = " 32 years";
- $is_married = true;
-
- echo $name;
- echo $age;
- echo $is_married
-?>
